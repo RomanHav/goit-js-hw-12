f4d8432c2d92c123cc6a0610de7c53b168841aae
RomanHav <roman14082004@gmail.com>
2024-04-05 13:23:44 +0300

    start project

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export let pagination = {
+  page: 1,
+  per_page: 15,
+};
+
+export async function fetchInfo(searchWord) {
+  try {
+    const params = {
+      q: searchWord,
+      key: '43138861-aa01fee7d014e33c8f4078e7d',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    };
+
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${params.key}&q=${params.q}&image_type=${params.image_type}&orientation=${params.orientation}&safesearch=${params.safesearch}&page=${pagination.page}&per_page=${pagination.per_page}`
+    );
+    // page += 1;
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+import { request, markup, renderMore, clearInput } from './js/render-functions';
+// import { button, } from './js/render-functions';
+
+const input = document.querySelector('.search');
+const button = document.querySelector('.button');
+const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-button');
+
+loadBtn.style.display = 'none';
+
+function renderImages(evt) {
+  evt.preventDefault();
+  const searchWord = request();
+  if (searchWord.length === 0) {
+    iziToast.error({
+      theme: 'dark',
+      message: 'The field must be fullfield',
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'topRight',
+      progressBarColor: '#B51B1B',
+    });
+  } else {
+    gallery.innerHTML = '';
+
+    markup();
+    clearInput();
+    loadBtn.style.display = 'block';
+    // input.value = '';
+  }
+}
+
+input.addEventListener('input', request);
+button.addEventListener('click', renderImages);
+loadBtn.addEventListener('click', renderMore);
